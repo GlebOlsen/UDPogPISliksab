fdd591b4e9eda124e58b28bc1214bddfd1f9bcbe
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2021-05-04 14:56:21 +0200

    Lavede UPD næsten færdig

--- ModelLibSlikskab/SensorData.cs
+++ ModelLibSlikskab/SensorData.cs
@@ -3,19 +3,21 @@ using System.Threading.Channels;
 
 namespace ModelLibSlikskab
 {
-    public class Class1
+    public class SensorData
     {
         public int SensorID { get; set; }
+        public int ReadingId { get; set; }
         public DateTime Time { get; set; }
         public bool IsOpen { get; set; }
         public byte[] ImageData { get; set; }
 
-        public Class1(int sensorID, DateTime time, bool isOpen, byte[] imageData)
+        public SensorData(int readingId,int sensorID, DateTime time, bool isOpen)
         {
             SensorID = sensorID;
             Time = time;
             IsOpen = isOpen;
-            ImageData = imageData;
+            //ImageData = imageData;
+            ReadingId = readingId;
         }
 
         public override string ToString()
--- UDPSlikskab/Program.cs
+++ UDPSlikskab/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using ModelLibSlikskab;
 
 namespace UDPSlikskab
 {
@@ -6,7 +11,41 @@ namespace UDPSlikskab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UdpClient receiveUdp = new UdpClient(9001);
+            IPEndPoint receiveEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            try
+            {
+                while (true)
+                {
+                    Byte[] receiveBytes = receiveUdp.Receive(ref receiveEndPoint);
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+
+                    Console.WriteLine("This message was sent from " +
+                                      receiveEndPoint.Address.ToString() +
+                                      " on their port number " +
+                                      receiveEndPoint.Port.ToString());
+
+                    Console.WriteLine(receivedData);
+
+                    string[] txtSplit = receivedData.Split(" ");
+                    for (int i = 0; i < txtSplit.Length; i++)
+                    {
+                        Console.WriteLine(txtSplit[i]);
+                    }
+
+                    SensorData sensorData = new SensorData(Convert.ToInt16(txtSplit[0]), Convert.ToInt16(txtSplit[1]),
+                        Convert.ToDateTime(txtSplit[2] + " " + txtSplit[3]), Convert.ToBoolean(txtSplit[4]));
+                    Thread.Sleep(3000);
+                    break;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
